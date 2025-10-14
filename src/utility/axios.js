9abdf607cc665c35352d8e30db0993b524da9d53
Abu-muaz <plshireseid@gmail.com>
2025-10-14 21:32:09 +0300

    deploy

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
-const serverPort = import.meta.env.PORT || 5000;
+// const serverPort = import.meta.env.PORT || 5000;
 
-export const axiosInstance = axios.create({
-  //local endpoint reference
-  baseURL: `http://localhost:${serverPort}/api/v1`,
+// export const axiosInstance = axios.create({
+//   //local endpoint reference
+//   baseURL: `http://localhost:${serverPort}/api/v1`,
 
   
+// });
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const axiosInstance = axios.create({
+  // Use the environment variable for the base URL
+  baseURL: `${API_BASE_URL}/api/v1`,
 });
