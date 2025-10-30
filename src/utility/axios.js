554d747dff273323e5faeb902987f9d9d7cb4d1d
Abu-muaz <plshireseid@gmail.com>
2025-10-30 08:21:40 +0300

    fix error

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -55,7 +55,8 @@ import axios from "axios";
 
 
 const baseURL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1";
+  import.meta.env.VITE_API_BASE_URL ||
+  "http://localhost:5000/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
